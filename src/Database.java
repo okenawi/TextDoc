1d09219939883da646d61730f7f75191650beb4e
omarebiary <omarebiary123@gmail.com>
2026-05-03 20:24:47 +0300

    Mongodb Setup completooo

--- src/Database.java
+++ src/Database.java
@@ -4,22 +4,12 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import javafx.animation.*;
-import javafx.application.Application;
 import javafx.geometry.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.scene.text.*;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.KeyCode;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import javafx.application.Platform;
-import javafx.scene.media.AudioClip;
 
 public class Database {
 
@@ -38,7 +28,7 @@ public class Database {
 
             // Create document
             Document user = new Document()
-                    .append("name", "Omar")
+                    .append("name", "seif alaaaa")
                     .append("age", 21)
                     .append("university", "Cairo University");
 
